99edc7f9b254fbb6404b55d72b6b46fe44ea1090
huoyijie <yijie.huo@foxmail.com>
2024-03-01 15:53:23 +0800

    add dept. tables

--- migrations/000001_init.down.sql
+++ migrations/000001_init.down.sql
@@ -1,11 +1,17 @@
 DROP TABLE IF EXISTS `s_apps`;
 
-DROP TABLE IF EXISTS `s_auth_tokens`;
-
 DROP TABLE IF EXISTS `s_staffs`;
+
+DROP TABLE IF EXISTS `s_auth_tokens`;
 
 DROP TABLE IF EXISTS `s_groups`;
 
-DROP TABLE IF EXISTS `s_users`;
-
 DROP TABLE IF EXISTS `casbin_rule`;
+
+DROP TABLE IF EXISTS `s_departments`;
+
+DROP TABLE IF EXISTS `s_dept_levels`;
+
+DROP TABLE IF EXISTS `s_dept_staffs`;
+
+DROP TABLE IF EXISTS `s_users`;
--- migrations/000001_init.up.sql
+++ migrations/000001_init.up.sql
@@ -12,23 +12,6 @@ CREATE TABLE IF NOT EXISTS `s_apps` (
 ) ENGINE=InnoDB AUTO_INCREMENT=100000 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
--- ink.s_auth_tokens definition
-
-CREATE TABLE IF NOT EXISTS `s_auth_tokens` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `access_token` varchar(64) NOT NULL,
-  `refresh_token` varchar(64) NOT NULL,
-  `app_id` int NOT NULL,
-  `account_id` int NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `s_auth_tokens_access_token_key` (`access_token`),
-  UNIQUE KEY `s_auth_tokens_refresh_token_key` (`refresh_token`),
-  KEY `s_auth_tokens_app_id_account_id_idx` (`app_id`,`account_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=100000 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-
 -- ink.s_staffs definition
 
 CREATE TABLE IF NOT EXISTS `s_staffs` (
@@ -47,6 +30,23 @@ CREATE TABLE IF NOT EXISTS `s_staffs` (
 ) ENGINE=InnoDB AUTO_INCREMENT=100000 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
+-- ink.s_auth_tokens definition
+
+CREATE TABLE IF NOT EXISTS `s_auth_tokens` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `access_token` varchar(64) NOT NULL,
+  `refresh_token` varchar(64) NOT NULL,
+  `app_id` int NOT NULL,
+  `account_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `s_auth_tokens_access_token_key` (`access_token`),
+  UNIQUE KEY `s_auth_tokens_refresh_token_key` (`refresh_token`),
+  KEY `s_auth_tokens_app_id_account_id_idx` (`app_id`,`account_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=100000 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
 -- ink.s_groups definition
 
 CREATE TABLE IF NOT EXISTS `s_groups` (
@@ -58,22 +58,7 @@ CREATE TABLE IF NOT EXISTS `s_groups` (
   `app_id` int NOT NULL,
   `active` tinyint(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`),
-  UNIQUE KEY `s_groups_app_id_type_name_key` (`app_id`,`type`,`name`),
-  KEY `s_groups_app_id_type_idx` (`app_id`,`type`)
-) ENGINE=InnoDB AUTO_INCREMENT=100000 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-
--- ink.s_users definition
-
-CREATE TABLE IF NOT EXISTS `s_users` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `email` varchar(128) NOT NULL,
-  `password` varchar(64) NOT NULL,
-  `active` tinyint(1) NOT NULL DEFAULT '1',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `s_users_email_key` (`email`)
+  UNIQUE KEY `s_groups_app_id_type_name_key` (`app_id`,`type`,`name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=100000 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
@@ -89,4 +74,58 @@ CREATE TABLE IF NOT EXISTS `casbin_rule` (
     `v4` varchar(255) NOT NULL DEFAULT '',
     `v5` varchar(255) NOT NULL DEFAULT '',
     PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=100000 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+-- ink.s_departments definition
+
+CREATE TABLE IF NOT EXISTS `s_departments` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `name` varchar(128) NOT NULL,
+  `owner_id` int NOT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `s_departments_name_key` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=100000 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+-- ink.s_dept_levels definition
+
+CREATE TABLE IF NOT EXISTS `s_dept_levels` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `dept_id` int NOT NULL,
+  `sub_dept_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `s_dept_levels_relation_key` (`dept_id`,`sub_dept_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=100000 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+-- ink.s_dept_staffs definition
+
+CREATE TABLE IF NOT EXISTS `s_dept_staffs` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `dept_id` int NOT NULL,
+  `staff_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `s_dept_staffs_relation_key` (`dept_id`,`staff_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=100000 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+-- ink.s_users definition
+
+CREATE TABLE IF NOT EXISTS `s_users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `email` varchar(128) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `s_users_email_key` (`email`)
 ) ENGINE=InnoDB AUTO_INCREMENT=100000 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
